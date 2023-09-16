074c37fa1bb013a559bf5c2de7e9d34f228f3c5f
jaimbh <115988572+jaimbh@users.noreply.github.com>
2023-09-16 16:48:58 +0530

    Update App.js
    added button to redirect to graphql

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,6 @@ const query = gql`
 		getTodos {
 			id
 			title
-			completed
 			user {
 				id
 				name
@@ -29,37 +28,43 @@ export default function App() {
 	return (
 		<>
 		<div className="container p-5">
-		<center><h3>Todos</h3></center><hr/>
-		<div className="px-5">
-			<table className="table">
-				<thead>
-					<tr>
-						<th scope="col">User ID</th>
-						<th scope="col">User</th>
-						<th scope="col">Username</th>
-						<th scope="col">Title</th>
-						<th scope="col">Email</th>
-						<th scope="col">Phone</th>
-						<th scope="col">Website</th>
-					</tr>
-				</thead>
-				<tbody>
-				
-				{data.getTodos.map((todo) => (
-				    <tr key={todo.id}>
-						<td>{todo?.user?.id}</td>
-						<td>{todo?.user?.name}</td>
-						<td>{todo?.user?.username}</td>
-						<td>{todo.title}</td>
-						<td>{todo?.user?.email}</td>
-						<td>{todo?.user?.phone}</td>
-						<td>{todo?.user?.website}</td>
-					</tr>
-				))}
+			<div className="d-flex justify-content-between align-items-center">
+				<h3 className="mb-0">Todos</h3>
+				<a href="http://192.168.11.128:8000/graphql" target="_blank">
+					<button className="btn btn-primary">GraphQL</button>
+				</a>
+			</div>
+			<hr/>
+			<div className="px-5">
+				<table className="table">
+					<thead>
+						<tr>
+							<th scope="col">User ID</th>
+							<th scope="col">User</th>
+							<th scope="col">Username</th>
+							<th scope="col">Title</th>
+							<th scope="col">Email</th>
+							<th scope="col">Phone</th>
+							<th scope="col">Website</th>
+						</tr>
+					</thead>
+					<tbody>
 					
-				</tbody>
-			</table>
-		</div>
+					{data.getTodos.map((todo) => (
+						<tr key={todo.id}>
+							<td>{todo?.user?.id}</td>
+							<td>{todo?.user?.name}</td>
+							<td>{todo?.user?.username}</td>
+							<td>{todo.title}</td>
+							<td>{todo?.user?.email}</td>
+							<td>{todo?.user?.phone}</td>
+							<td>{todo?.user?.website}</td>
+						</tr>
+					))}
+						
+					</tbody>
+				</table>
+			</div>
 		</div>
 		</>
 	);
